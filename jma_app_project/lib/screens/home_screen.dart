ea8281627a6abd3d058b34b8546b6b1cca14cfaf
Bankaster <miguelroto1998@gmail.com>
2023-12-21 11:03:37 +0100

    Scrollable Home Screen

--- jma_app_project/lib/screens/home_screen.dart
+++ jma_app_project/lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const double backgroundPhotoHeight = 350;
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({
     super.key,
@@ -7,19 +9,62 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-         automaticallyImplyLeading: false,
-        title: const Text("Home Screen"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Conpendium Screen"),
-          onPressed: () {
-            Navigator.of(context).pushNamed("/ConpendiumScreen");
-          },
-        ),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: SizedBox(
+              height: backgroundPhotoHeight + 20,
+              width: screenSize.width,
+              child: const Image(
+                image: NetworkImage(
+                    'https://raw.githubusercontent.com/gadhagod/hyrule-compendium-demo/main/images/hyrule-compendium-meta.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: screenSize.width,
+            height: screenSize.height,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    height: backgroundPhotoHeight,
+                    width: screenSize.width,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                    height: 1100,
+                    width: screenSize.width,
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          child: const Text("Conpendium Screen"),
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushNamed("/ConpendiumScreen");
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
