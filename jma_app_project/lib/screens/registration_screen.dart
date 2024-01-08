1c0eac1e3deefd459dd7d0ccedebb7936523927c
Joan <jdamiaros@gmail.com>
2024-01-08 18:20:47 +0100

    Avanços en la registration screen

--- jma_app_project/lib/screens/registration_screen.dart
+++ jma_app_project/lib/screens/registration_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jma_app_project/widgets/general_widgets/home_button.dart';
 import 'package:jma_app_project/widgets/general_widgets/important_tittle.dart';
-import 'package:jma_app_project/widgets/register_widgets/register_button.dart';
+
 
 class RegistrationScreen extends StatelessWidget {
   const RegistrationScreen({super.key});
@@ -23,9 +23,18 @@ class RegistrationScreen extends StatelessWidget {
         centerTitle: true,
         leading: const HomeButton(),
       ),
-      body: const Stack(
+      body:  Stack(
         children: [
+          Positioned(
+            top: 100,
+            left: 20,
          // RegisterButton(gameElement: gameElement)
+           child: ElevatedButton(
+           onPressed:() {
+           },
+           child: const Text('Generate Now'),
+           ),
+          ),
         ],
       ),
     );
